271c4953598d64b3b18aa1c2c19ee16823568451
Jörn Düfelsiek <joern_timon.duefelsiek@fh-bielefeld.de>
2021-11-21 12:54:00 +0100

    Refactoring

--- src/proxy.py
+++ src/proxy.py
@@ -3,9 +3,11 @@ import requests
 from itertools import cycle
 from random import randint
 
+_HEADER = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+
 def __parse_proxies():
     url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
+    response = requests.get(url, headers=_HEADER)
     parser = fromstring(response.text)
     proxies = list()
     for i in parser.xpath('//tbody/tr')[:100]:
@@ -14,11 +16,15 @@ def __parse_proxies():
             proxies.append(proxy)
     return proxies
 
+def _proxy_test(proxy):
+    return requests.get('https://www.google.com/', headers=_HEADER, proxies={'http://':'http://'+proxy, 'https://':'https://'+proxy}).status_code < 400
+
 def get_proxy():
-    proxies = __parse_proxies()
-    if len(proxies) >0:
-        proxy_num = randint(0, len(proxies)-1)
-    else:
-        get_proxy()
-    prox = proxies[proxy_num]
-    return prox
+    while True:
+        proxies = __parse_proxies()
+        if len(proxies) >0:
+            proxy = proxies[randint(0, len(proxies)-1)]
+            if _proxy_test(proxy):
+                return proxy
+            else:
+                print('Proxy not working') 
